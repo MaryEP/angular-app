73dd3c472f5002334f2df824424bb7a3437b8525
MaryEP <pringle67@hotmail.com>
2016-08-18 19:41:14 -0700

    main html,scss,js, about and transitions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,27 +11,23 @@
   angular.module('projectsApp')
     .controller('MainCtrl', function($scope, current, $filter) {
       $scope.saledate = $filter('date')(new Date(),'MM/dd/yy');
+      $scope.publisher = 'Harper Collins';
       $scope.books = current.query({
         saledate: $scope.saledate
       });
 
       $scope.findBooks = function (){
+        $scope.warning = false;
         $scope.books = current.query({
           publisher: 'Harper Collins',
           saledate: $scope.saledate,
           authorname: $scope.authorname,
           title: $scope.title
         });
-      };
-
-
-      $scope.changepublisher = function (){
         if ($scope.publisher !=='Harper Collins') {
-          $scope.publisher = {
-            'warning': true
+           $scope.warning = true;
+           $scope.publisher = 'Harper Collins';
+      }
       };
-    }
-    };
 
-
-  });
+});
